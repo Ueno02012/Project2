53b22bf2c82ff383f51e8918de4e40cd8e268cac
Ueno02012 <k023g0064@g.neec.ac.jp>
2024-10-16 12:22:26 +0900

    test

--- main.cpp
+++ main.cpp
@@ -1,22 +1,19 @@
 #include<stdio.h>
-int Recursive(int n) {
-	if (n <= 1) {
-		return 100;
-	}
-	return (Recursive(n-1)*2)-50;
+
+void PrintHelloWorld(void) {
+	printf("HelloWorld\n");
 }
 
-int main() {
-	int n = 10;
-	int ander = 1072;
-	int result;
+int main(void) {
 
-	for (int i = 1; i < 11; i++) {
-		result=Recursive(i);
-		printf("Å’á’À‹à : %d\n", ander);
-		printf("Ä‹A“I‚È’À‹à‘ÌŒn : %d\n\n", result);
-		ander += 1072;
-	}
-	return(0);
+	void(*pfunc)();
+
+	pfunc = PrintHelloWorld;
+	printf("PrintHelloWorldã®ã‚¢ãƒ‰ãƒ¬ã‚¹=%p\n", PrintHelloWorld);
+	printf("pfuncã®å†…å®¹ = %p\n", *pfunc);
+	pfunc();
+
+
+	return 0;
 }
 
