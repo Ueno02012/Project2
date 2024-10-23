138c2fdb06cec7ca86301e434d07eb7cada04b94
Ueno02012 <k023g0064@g.neec.ac.jp>
2024-10-23 19:00:53 +0900

    test

--- main.cpp
+++ main.cpp
@@ -4,19 +4,19 @@
 
 int Recursive(int n) {
 	if (n <= 1) {
-		return (1);
+		return 100;
 	}
-	return (n * 2 - 50);
+	return (n * Recursive(n-1));
 }
 
 int main() {
 
 	SetConsoleCP(65001);
+	SetConsoleOutputCP(65001);
 	int n = 100;
 	int result;
-
 	result = Recursive(n);
-	printf("%dの階乗=%d", n, result);
+	printf("%dの階乗=\n", result);
 	return(0);
 
 
