50cbcc7d35827b80d8c1c058941e6bc131181e8a
Ueno02012 <k023g0064@g.neec.ac.jp>
2024-11-13 18:33:28 +0900

    test

--- main.cpp
+++ main.cpp
@@ -1,16 +1,6 @@
 #include<stdio.h>
-#include"Enemy.h"
 int main() {
-    Enemy enemy;
-    while (true) {
-        enemy.Update();
-        int s;
-        printf("0で次のフェーズ、１でループを抜ける\n");
-        scanf_s("%d", &s);
-        if (s == 1) {
-            break;
-        }
-    }
+
 
     return 0;
 }
